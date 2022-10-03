c2153efc81cab8cec8f4eef7065f68f689f69a3b
Anekant <Anekantjainsagar@gmail.com>
2022-10-03 23:17:44 +0530

    Changes

--- index.js
+++ index.js
@@ -1,54 +1,80 @@
-const express = require('express')
-const app = express()
-const mongoose = require('mongoose')
-const bodyParser = require('body-parser')
-const Contact = require('./model/model')
-const Message = require('./model/msgModel')
-const port = process.env.PORT || 5000
-const cors = require('cors')
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const Contact = require("./model/model");
+const Post = require("./model/PostModel");
+const port = process.env.PORT || 5000;
+const cors = require("cors");
 
-app.use(cors())
-mongoose.connect('mongodb+srv://blog:blog1234@cluster0.uv0j1qr.mongodb.net/BlogApp').then(()=>{
+app.use(cors());
+mongoose
+  .connect("mongodb+srv://blog:blog1234@cluster0.uv0j1qr.mongodb.net/BlogApp")
+  .then(() => {
     console.log("Connection successful...");
-}).catch((err)=>{
+  })
+  .catch((err) => {
     console.log(err);
-})
+  });
 
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-app.get('/',(req,res)=>{
-    res.send('Hello world')
-})
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
 
-app.post('/contact',(req,res)=>{
-    const {name,email,message} = req.body
+app.post("/contact", (req, res) => {
+  const { name, email, message } = req.body;
 
-    const contact = new Contact({name, email, message})
-    contact.save().then(()=>{
-        res.status(200).json({Success:true,message:contact});
-    }).catch((err)=>{
-        res.status(204).json({Success:false,message:err});
+  const contact = new Contact({ name, email, message });
+  contact
+    .save()
+    .then(() => {
+      res.status(200).json({ Success: true, message: contact });
     })
-})
+    .catch((err) => {
+      res.status(204).json({ Success: false, message: err });
+    });
+});
 
-app.post('/addBlog',(req,res)=>{
-    const {title,message} = req.body
+app.post("/addBlog", (req, res) => {
+  const { title, description, image } = req.body;
 
-    const msg = new Message({title, message})
-    msg.save().then(()=>{
-        res.status(200).json({Success:true,message:msg});
-    }).catch((err)=>{
-        res.status(204).json({Success:false,message:err});
+  const post = new Post({ title, description, image });
+  post
+    .save()
+    .then(() => {
+      res.status(200).json({ Success: true, message: msg });
     })
-})
+    .catch((err) => {
+      res.status(204).json({ Success: false, message: err });
+    });
+});
 
-app.get('/getBlog',(req,res)=>{
-    Message.find((err,data)=>{
-        res.send(data)
-    })
-})
+app.get("/getBlog", async (req, res) => {
+  var { page, size } = req.query;
 
-app.listen(port,()=>{
-    console.log(`Tuffle is running at port ${port}`);
-})
+  if (!page) {
+    page = 1;
+  }
+  if (!size) {
+    size = 10;
+  }
+
+  const limit = parseInt(size);
+
+  const totalPosts = await Post.find();
+  const filteredPosts = await Post.find().limit(limit);
+  res.send({
+    page,
+    size,
+    posts: filteredPosts,
+    NoOfPosts: filteredPosts.length,
+    totalNoPosts: totalPosts.length,
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Tuffle is running at port ${port}`);
+});
